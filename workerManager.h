2f144e30f74b6e55a7f28f2dbfb79a7f13ef16f0
慧超 朱 <zhuhuichao35@outlook.com>
2023-08-25 21:47:14 +0800

    添加项目文件。

---
+++ workerManager.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<iostream>
+using namespace std;
+
+class WorkerManager
+{
+public:
+	WorkerManager();
+	// 菜单
+	void ShowMenu();
+	//退出管理程序
+	void ExitSystem();
+
+	~WorkerManager();
+};
